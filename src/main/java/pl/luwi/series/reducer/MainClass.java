48922b99c7bdbf0d2f9a7b8c110f4ebab300548e
Maedeh <maedeh.hm21@gmail.com>
2017-05-17 13:27:42 -0400

    Code works and creates smaller number of points, but I cannot wrote a kml file for that yet.

--- src/main/java/pl/luwi/series/reducer/MainClass.java
+++ src/main/java/pl/luwi/series/reducer/MainClass.java
@@ -1,10 +1,14 @@
 package pl.luwi.series.reducer;
 
 
+import de.micromata.opengis.kml.v_2_2_0.*;
+import de.micromata.opengis.kml.v_2_2_0.Polygon;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
-
-import static java.util.Arrays.asList;
 import static pl.luwi.series.reducer.PointImpl.p;
+
 
 
 /**
@@ -15,13 +19,47 @@ public class MainClass {
     public static void main(String[] args){
 
 
-        List myList = asList(p(43.048096837000053, -81.129290106999917), p(43.044616697000038, -81.127565861999926), p(43.044120331000045, -81.127319941999929), p(43.043049488000065, -81.126737318999915));
+        Kml kml = Kml.unmarshal(new File("C:\\Users\\maedeh.hesabgar\\Desktop\\london\\london\\ca_on_neighbourhood_london_stoneybrook.kml"));
+        final Placemark placemark = (Placemark) kml.getFeature();
+        final Polygon polygon = (Polygon) placemark.getGeometry();
+        final Boundary outerBoundaryIs = polygon.getOuterBoundaryIs();
+        LinearRing linearRing = outerBoundaryIs.getLinearRing();
+        List<Coordinate> coordinates = linearRing.getCoordinates();
+        System.out.print(coordinates.size());
 
-        System.out.println(myList);
+        List<Point> myPoints= new ArrayList<Point>();
+        for(Coordinate coordinate : coordinates) {
+            if(coordinate != null) {
+                System.out.println("Longitude: " +  coordinate.getLongitude());
+                System.out.println("Latitude : " +  coordinate.getLatitude());
+                System.out.println("");
+                myPoints.add(p(coordinate.getLatitude(),coordinate.getLongitude()));
+            }
+        }
 
-        Double epsilon = EpsilonHelper.avg(EpsilonHelper.deviations(myList));
-        List<Point> reduced1 = SeriesReducer.reduce(myList, epsilon);
-        System.out.println(reduced1);
+        myPoints.remove(myPoints.size()-1);
+
+        Double epsilon = EpsilonHelper.avg(EpsilonHelper.deviations(myPoints));
+        List<Point> reduced1 = SeriesReducer.reduce(myPoints, epsilon);
+        reduced1.add(reduced1.get(0));
+        System.out.println(reduced1.size());
+
+
+        final Kml createdKml = new Kml();
+        de.micromata.opengis.kml.v_2_2_0.Point p2 = createdKml.createAndSetPlacemark()
+                .withName("FirstTest").withOpen(Boolean.TRUE)
+                .createAndSetPoint();
+
+        for(Point p:reduced1){
+            p2.addToCoordinates(p.getY(), p.getX());
+
+        }
+
+        try {
+            createdKml.marshal(new File("HelloKml.kml"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
-
 }
+
